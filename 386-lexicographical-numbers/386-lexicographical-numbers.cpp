f117413499510de6e035d91a6d977df2152b981f
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-05 15:37:32 +0530

    Time: 22 ms (28.24%), Space: 12.1 MB (33.26%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
+    vector<int>ans;
+    void dfs(int i,int n)
+    {
+        if(i>n) return;
+        ans.push_back(i);
+        for(int j=0;j<10;j++)
+            dfs(10*i+j,n);
+    }
     vector<int> lexicalOrder(int n) {
-        vector<string>temp;
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-            temp.push_back(to_string(i+1));
-        
-        sort(begin(temp),end(temp));
-        for(int i=0;i<n;i++)
-            ans.push_back(stoi(temp[i]));
-        
+      for(int i=1;i<10;i++) dfs(i,n);
         return ans;
     }
 };
