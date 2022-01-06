ad9843e2efb65fea3963be5ee013700423393862
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-06 11:34:49 +0530

    Time: 71 ms (5.10%), Space: 10.1 MB (60.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -3,12 +3,12 @@ public:
     bool carPooling(vector<vector<int>>& trip, int cap) {
         int n=trip.size();
      
-        vector<int>store(10000,0);
+        vector<int>store(1001,0);
         for(int i=0;i<n;i++)
         {
             for(int j=trip[i][1];j<trip[i][2];j++) store[j]+=trip[i][0];
         }
-        for(int i=0;i<=9999;i++)
+        for(int i=0;i<=1000;i++)
             if(store[i]>cap)
                 return false;
         
