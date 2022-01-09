64302087d8a183e332774c380e76a3aca5317d5a
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-09 13:59:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (48.40%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    bool isRobotBounded(string ins) {
+        bool n=true,s=false,e=false,w=false;
+        int x=0,y=0;
+        for(auto i:ins)
+        {
+            if(i=='G')
+            {
+                if(n)
+                    y++;
+                else if(w)
+                    x--;
+                else if(s)
+                    y--;
+                else
+                    x++;
+            }
+            else if(i=='R')
+            {
+                if(n)
+                {
+                    n=false;
+                    e=true;
+                }
+                else if(w)
+                {
+                    n=true;
+                    w=false;
+                }
+                else if(e)
+                {
+                    e=false;
+                    s=true;
+                }
+                else{
+                    s=false;
+                    w=true;
+                }
+                    
+            }
+            else{
+                if(n)
+                {
+                    n=false;
+                    w=true;
+                }
+                else if(e)
+                {
+                    e=false;
+                    n=true;
+                }
+                else if(w)
+                {
+                    w=false;
+                    s=true;
+                }
+                else
+                {
+                    s=false;
+                    e=true;
+                }
+            }
+        }
+        if(x==0 and y==0) return 1;
+        if(n) return 0;
+        return 1;
+    }
+};
