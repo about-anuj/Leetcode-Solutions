cab9a47261a327b366d649cd9faa552e90fd93ee
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-10 10:43:34 +0530

    Time: 5 ms (17.36%), Space: 7 MB (25.84%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -3,65 +3,17 @@ public:
     string addBinary(string a, string b) {
         int carry=0;
         string ans="";
-        int sz=min(a.length(),b.length());  // this is maximum iteration we can done in loop
-        int last=max(a.length(),b.length());  //last point to start addition
         int x=a.length()-1,y=b.length()-1;
-        for(int i=0;i<sz;i++)
+        while(x>=0 or y>=0)
         {
-            if(a[x]=='1' and b[y]=='1')
-            {
-                if(!carry)
-                    ans='0'+ans;
-                else
-                    ans='1'+ans;
-               carry=1;
-            }
-            else if(a[x]=='0' and b[y]=='0')
-            {
-                if(carry){
-                    ans='1'+ans;
-                carry=0;
-                }
-                else
-                    ans='0'+ans;
-            }
-            else{
-                if(carry)
-                    ans='0'+ans;
-                else
-                    ans='1'+ans;
-            }
-           // cout<<ans<<endl;
-            x--,y--;
-        }
-       
-        string s=(x==-1?b:a);
-        last=max(x,y);
-        
-        for(;last>=0;last--)
-        {
-            if(carry)
-            {
-                if(s[last]=='1'){
-                ans='0'+ans;
-                }
-                else
-                  {
-                    ans='1'+ans;
-                   carry=0;
-                }
-                
-            }
-            else{
-                if(s[last]=='0')
-                    ans='0'+ans;
-                else
-                    ans='1'+ans;
-            }
+            int sum=carry;
+            sum+=(x>=0?a[x--]-'0':0);
+            sum+=(y>=0?b[y--]-'0':0);
             
+            ans=to_string(sum%2)+ans;
+            carry=sum/2;
         }
-        if(carry)
-            ans='1'+ans;
+        if(carry) ans="1"+ans;
         return ans;
     }
 };
