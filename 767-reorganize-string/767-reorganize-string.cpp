161f90c2c06b49c4d991bcf2c8a320ffc62fd7d4
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-19 12:42:30 +0530

    Time: 9 ms (5.92%), Space: 6.4 MB (33.88%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char,int>mp;
+        int n=s.size();
+        string ans="";
+        int mx=INT_MIN;
+        for(auto i:s)
+        {
+            mp[i]++;
+            mx=max(mx,mp[i]);
+        }
+        if(mx>(n+1)/2) return "";
+       priority_queue<pair<int,char>>pq;
+        for(auto i:mp)
+        {
+            pq.push({i.second,i.first});
+        }
+        int i=0;
+        pair<int,int>prev=pq.top(); pq.pop();
+        ans+=prev.second;
+        prev.first--;
+        while(!pq.empty())
+        {
+            pair<int,char>curr=pq.top();
+            pq.pop();
+            int x=curr.first;
+            char val=curr.second;
+            ans+=val;
+            if(prev.first>0)pq.push({prev.first,prev.second});
+            prev={x-1,val};
+        }
+        return ans;
+    }
+};
