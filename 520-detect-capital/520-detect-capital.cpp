f625b2effe30867db41cf2c0826af753c0422aa4
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-24 12:03:14 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (19.00%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     bool detectCapitalUse(string w) {
-        
+       bool first=isupper(w[0]),allcap=true,allsmall=true;
         for(int i=1;i<w.size();i++)
         {
-            if((isupper(w[1])!=isupper(w[i])) or (islower(w[0]) and isupper(w[i])))
-                return false;
+            if(isupper(w[i]))
+                allsmall=false;
+            if(islower(w[i]))
+                allcap=false;
         }
-        return true;
+        //AAA
+       if(first and allcap) return 1;
+        // Aaa
+        if(first and allsmall) return 1;
+        //aaa
+        if(allsmall) return 1;
+        
+        return 0;
     }
 };
