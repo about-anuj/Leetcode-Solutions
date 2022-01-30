35a9ccb6f93baab7c05c7bfceb54f34c65a66de9
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-01-30 10:06:11 +0530

    Time: 324 ms (63.64%), Space: 286.7 MB (22.73%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     vector<int> maxScoreIndices(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>pre(n+1,0),suff(n+1,0);
-        for(int i=1;i<n+1;i++)
-          {
-           if(nums[i-1]==0)
-               pre[i]=pre[i-1]+1;
-           else
-               pre[i]=pre[i-1];
-           // cout<<pre[i]<<" ";
-           }
-    for(int i=n-1;i>=0;i--)
-    {
-        if(nums[i]==1)
-            suff[i]=suff[i+1]+1;
-        else
-            suff[i]=suff[i+1];
-       
-    }
-       
-        vector<pair<int,int>>v;
-        for(int i=0;i<=n;i++)
-        {
-            v.push_back({pre[i]+suff[i],i});
+        int rightOne=accumulate(begin(nums),end(nums),0),leftZero=0,maxScore=0;
+        vector<int>res;
+        for(int i=0;i<=nums.size();i++){
+            
+            if(rightOne+leftZero > maxScore){
+                maxScore=rightOne+leftZero;
+                res.clear();
+                res.push_back(i);
+            }
+            
+            else if(rightOne+leftZero == maxScore) res.push_back(i);
+            
+            if(i!=nums.size()){
+                if(nums[i]==0)leftZero++;
+                if(nums[i]==1)rightOne--;
+            }
         }
-        sort(begin(v),end(v));
-       // reverse(begin(v),end(v));
-        int i=n,x=v[n].first;
-        vector<int>ans;
-        while(i>=0 and v[i].first==x)
-        {
-            ans.push_back(v[i].second);
-            //cout<<v[i].first<<" ";
-            i--;
-        }
-        //sort(begin(ans),end(ans));
-        return ans;
+        return res;
     }
 };
