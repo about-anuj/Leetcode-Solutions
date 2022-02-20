a78b4e8eee7ec19098e925f6dd78a777352db95b
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-02-20 13:54:21 +0530

    Time: 195 ms (6.58%), Space: 28.7 MB (7.89%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -14,23 +14,25 @@ public:
         //sort the intervals on the basis of cpmprator
         sort(begin(intervals),end(intervals),cmp);
         
-        vector<vector<int>>v;
-        //push the first element for referemce
-        v.push_back(intervals[0]);
+        int x=intervals[0][0],y=intervals[0][1],ans=1;
         for(int i=1;i<intervals.size();i++)
         {
-            vector<int>temp=v.back();
+         
             //if below condition satisfied then it is ok dont push into vector
             //The interval [a, b) is covered by the interval [c, d) if and only if c <= a and 
             //b <= d.
-            if(temp[0]<=intervals[i][0] and temp[1]>=intervals[i][1])
+            if(x<=intervals[i][0] and y>=intervals[i][1])
             {
                continue;
             }
             //if above condition not satisfied then push into v
             else
-                v.push_back(intervals[i]);
+                {
+                ans++;
+                x=intervals[i][0];
+                y=intervals[i][1];
+            }
         }
-        return (int)v.size();
+        return ans;
     }
 };
