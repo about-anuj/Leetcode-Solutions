cceb28772954125fd05b7b98a78f4d3317fb288e
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-02-20 13:57:09 +0530

    Time: 34 ms (51.97%), Space: 11.4 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,16 +3,13 @@ public:
     //if first element is less then it is ok to sort
     //but if first element is equal then sort on the
     //basis of gap or put larger range vector first
-    bool static cmp(vector<int>a,vector<int>b){
-        if(a[0]<b[0]) return 1;
-        else if(a[0]>b[0]) return 0;
-        
-        if(a[1]-a[0]>b[1]-b[0]) return 1;
-        return 0;
-    }
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         //sort the intervals on the basis of cpmprator
-        sort(begin(intervals),end(intervals),cmp);
+        sort(begin(intervals),end(intervals),
+             [](vector<int>& a, vector<int>& b)
+             {
+                 return (a[0]==b[0])?(a[1]>b[1]):(a[0]<b[0]); 
+             });
         
         int x=intervals[0][0],y=intervals[0][1],ans=1;
         for(int i=1;i<intervals.size();i++)
