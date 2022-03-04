9a643d3663133dd3d8410777cfbdc73930160804
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-03-04 17:01:16 +0530

    Time: 8 ms (99.34%), Space: 11.5 MB (31.96%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,74 +10,26 @@
  */
 class Solution {
 public:
- 
-    void reverse(ListNode* head){
-        ListNode* pre=NULL,*next=NULL;
-        while(head)
-        {
-            next=head->next;
-            head->next=pre;
-            pre=head;
-            head=next;
-        }
-        
-    }
+    
     
     
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* t=head;
-        vector<pair<ListNode*,ListNode*>>v;
-       // v.push_back({NULL,NULL});
-        while(t){
-            int j=k;
-            ListNode* st=t,*end;
-            while(t and j--){
-                end=t;
-                t=t->next;
-            }
-            end->next=NULL;
-            
-          if(!t and j) end=NULL;
-            
-         v.push_back({st,end});
+       ListNode* temp=head;
+        for(int i=0;i<k;i++){
+            if(!temp) return head;
+            temp=temp->next;
         }
         
-        
-        //if size of list is less than k
-         if(v[0].second==NULL)
-         {
-             reverse(v[0].first);
-             return v[0].second;
-         }
-        
-    
-        reverse(v[0].first);
-        swap(v[0].first,v[0].second); //as list became reverse so swap first and last node
-        
-        for(int i=1;i<v.size();i++)
+        ListNode* curr=head,*pre=NULL,*next=NULL;
+        for(int i=0;i<k;i++)
         {
-            // it means it is last node and its length is less then k
-            if(v[i].second==NULL){
-                v[i-1].second->next=v[i].first;
-                break;
-            }
-            
-            reverse(v[i].first);
-            swap(v[i].first,v[i].second);
-            //cout<<v[i-1].second->val<<" "<<v[i].first->val<<endl;
-            v[i-1].second->next=v[i].first;
-            
+            next=curr->next;
+            curr->next=pre;
+            pre=curr;
+            curr=next;
         }
-        // for(auto i:v)
-        // {
-        //     ListNode* t=i.first;
-        //    while(t)
-        //    {
-        //        cout<<t->val<<" ";
-        //        t=t->next;
-        //    }
-        //     cout<<endl;
-        // }
-        return v[0].first;
+       
+        head->next=reverseKGroup(curr,k);
+        return pre;
     }
 };
