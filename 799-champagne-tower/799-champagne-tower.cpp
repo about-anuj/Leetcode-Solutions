ffdd9c600868007266c1eced86cb6c024d2d84dd
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-03-04 14:38:02 +0530

    Time: 8 ms (73.16%), Space: 5.9 MB (91.14%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    double dp[101][101];
-    double solve(int filled,int r,int c){
-        if(r==0 and c==0) return filled;
-        if(c<0 or c>r or r<0) return 0.0;
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[105][105]={0.0};
+        dp[0][0]=double(poured); //all glasses will go through first glass;
         
-        if(dp[r][c]>-1.0)
-            return dp[r][c];
+        for(int i=0;i<100;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+              //  double q=(dp[i][j]-1.0)/2.0;
+                if(dp[i][j]>=1)
+                {
+                    dp[i+1][j]+=(dp[i][j]-1.0)/2.0;
+                    dp[i+1][j+1]+=(dp[i][j]-1.0)/2.0;
+                    dp[i][j]=1;
+                }
+            }
+        }
         
-        double x=max((solve(filled,r-1,c-1)-1)/2,double(0));
-        double y=max((solve(filled,r-1,c)-1)/2,double(0));
-        
-        return dp[r][c]=x+y;
-    }
-    double champagneTower(int filled, int r, int c) {
-       memset(dp,-1.0,sizeof(dp));
-       return min(solve(filled,r,c),double(1));
+        return dp[query_row][query_glass];
     }
 };
