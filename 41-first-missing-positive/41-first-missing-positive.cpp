26c8be721c1e424e6961638f04da8993ba89a31d
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-03-09 17:10:42 +0530

    Time: 120 ms (99.61%), Space: 83.1 MB (41.82%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n=(int)nums.size(),c=0,mn=INT_MAX;
-//         //c will measure number of non [positive] integers including 0
-//         for(auto i:nums){
-//             if(i<=0)
-//                 c++;
-//             else
-//                 mn=min(mn,i);
-//         }
-//         if(mn!=1)
-//             return 1;
+
+        //set the elements which are out of range
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<=0)
+                nums[i]=n+1;
+        }
+        //
+        for(int i=0;i<n;i++){
+            if(abs(nums[i])<=n and nums[abs(nums[i])-1]>0)
+                nums[abs(nums[i])-1]*=-1;
+        }
         
-//         //now answer will be from 1 to n-c;
-        
-        unordered_map<int,int>mp;
-        for(auto i:nums)
-            mp[i]++;
-        
-        for(int i=1;i<=n+1;i++)
-            if(!mp.count(i))
-                return i;
-        
-        return 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+                return i+1;
+        }
+         return n+1;
     }
 };
