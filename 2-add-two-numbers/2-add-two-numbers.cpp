6f49ad6fc35c607ae2fbb3cedafadfd5820e6ddf
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-03-10 11:21:06 +0530

    Time: 120 ms (5.34%), Space: 71.5 MB (11.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -16,25 +16,33 @@ public:
         int carry=0;
         while(l1 or l2){
             int val=carry;
+            //if l1 is not null then add that node value
             if(l1)
             {
                 val+=l1->val;
                 l1=l1->next;
             }
+            //similarly as l1
             if(l2){
                 val+=l2->val;
                 l2=l2->next;
             }
-           // int val=l1->val+l2->val+carry;
+           
+            //calculation for carry
             carry=val/10;
             val=val%10;
+            
+            //make node and link
             auto cur=new ListNode(val);
             t->next=cur;
             t=cur;
             
+            //if both become null and carry exist that means we have
+            //to add carry value so add that and this will be our last value
             if(!l1 and !l2 and carry>0){
                auto cur=new ListNode(carry);
                t->next=cur;  
+                
             }
         }
         
