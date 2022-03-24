004e6eebee30e102ccb45c4ebf2a213dde179a5a
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-03-24 08:43:57 +0530

    Time: 144 ms (20.42%), Space: 41.9 MB (57.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& p, int limit) {
-        unordered_map<int,int>mp;
+       // unordered_map<int,int>mp;
         sort(begin(p),end(p));
         int a=0,b=p.size()-1,ans=0;
         while(a<=b)
         {
-             if(p[a]+p[b]>limit)
-                b--;
-            else 
-                a++,b--;
+             if(p[a]+p[b]<=limit)
+               a++;
+            
+            b--;
             
             ans++;
         }
