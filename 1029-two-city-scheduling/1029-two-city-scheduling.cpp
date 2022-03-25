f4437b57d3b01ef9d64b4082c338b443c5300387
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-03-25 22:34:16 +0530

    Time: 15 ms (14.01%), Space: 10.1 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-    int ans=INT_MAX;
-    int dp[101][101][101];
-    int calc(int i, int n, vector<vector<int>>& costs, int cur,int a, int b)
+    bool static cmp(vector<int>a,vector<int>b)
     {
-        if(i==n){
-          return cur;
-        }
-        
-        if(dp[a][b][i]!=-1) return dp[a][b][i];
-     
-        if(a==n/2)
-        {
-            for(int j=i;j<n;j++)
-                cur+=costs[j][1];
-            
-            return cur;
-        }
-        
-        if(b==n/2)
-        {
-            for(int j=i;j<n;j++)
-               cur+=costs[j][0];
-            
-            return cur;
-        }
-        
-      return  dp[a][b][i]=min(calc(i+1,n,costs,cur,a+1,b)+costs[i][0],
-                              calc(i+1,n,costs,cur,a,b+1)+costs[i][1]);
-        
-       
+        return a[0]-a[1]<b[0]-b[1];
     }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        memset(dp,-1,sizeof(dp));
-        return calc(0,costs.size(),costs,0,0,0);
-     //  return  ans;
+        sort(begin(costs),end(costs),cmp);
+        
+        int ans=0;
+        for(int i=0;i<costs.size()/2;i++)
+            ans+=costs[i][0];
+        
+        for(int i=costs.size()/2;i<costs.size();i++)
+            ans+=costs[i][1];
+        
+        return ans;
     }
 };
