98ebd4b43690164f10743d53b8c03c5a5f6444a2
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-04-11 10:33:17 +0530

    Time: 52 ms (42.74%), Space: 14 MB (40.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
      int m=grid.size(),n=grid[0].size(),c=0;
-       deque<int>dq;
-        for(auto i:grid)
-            for(auto j:i)
-                dq.push_back(j);
-        
+      
         k%=(m*n);
-        while(k--)
-        {
-            int b=dq.back();
-            dq.pop_back();
-            dq.push_front(b);
-        }
         
+        vector<vector<int>>ans(m,vector<int>(n,0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                grid[i][j]=dq.front();
-                dq.pop_front();
+                int cur=(n*i)+j;
+                
+                int newpos=(cur+k)%(m*n);
+                int r=newpos/n;
+                int c=newpos%n;
+                ans[r][c]=grid[i][j];
             }
         }
-        return grid;
+        return ans;
     }
 };
