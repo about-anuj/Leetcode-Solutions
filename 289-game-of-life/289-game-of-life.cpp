c612de22363c6db637d1c59ff5f37c49e641ea93
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-04-12 09:51:47 +0530

    Time: 4 ms (26.80%), Space: 6.9 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>>v=board;
         int m=board.size(),n=board[0].size();
         for(int i=0;i<m;i++)
         {
@@ -12,48 +11,56 @@ public:
                 int c=0;
                 //up
                 
-                if(i-1>=0 and board[i-1][j]==1)
+                if(i-1>=0 and (board[i-1][j]==1 or board[i-1][j]==3))
                     c++;
                 //left
                 
-                if(j-1>=0 and board[i][j-1]==1)
+                if(j-1>=0 and (board[i][j-1]==1 or board[i][j-1]==3))
                     c++;
                 //right
                 
-                if(j+1<n and board[i][j+1]==1)
+                if(j+1<n and (board[i][j+1]==1 or board[i][j+1]==3))
                     c++;
                 //down
-                if(i+1<m and board[i+1][j]==1)
+                if(i+1<m and (board[i+1][j]==1 or board[i+1][j]==3))
                     c++; 
                 
                 //ur
-                if(j+1<n and i-1>=0  and board[i-1][j+1]==1)
+                if(j+1<n and i-1>=0  and (board[i-1][j+1]==1 or board[i-1][j+1]==3))
                     c++;
                
                 //rd
                 
-                if(j+1<n and i+1<m and board[i+1][j+1]==1)
+                if(j+1<n and i+1<m and (board[i+1][j+1]==1 or board[i+1][j+1]==3))
                     c++;
                 //dl
                  
-                if(j-1>=0 and i+1<m and board[i+1][j-1]==1)
+                if(j-1>=0 and i+1<m and (board[i+1][j-1]==1 or board[i+1][j-1]==3))
                     c++;
                 //lu
-                if(j-1>=0 and i-1>=0 and board[i-1][j-1]==1)
+                if(j-1>=0 and i-1>=0 and ( board[i-1][j-1]==1 or board[i-1][j-1]==3))
                     c++;
                 
                 
                 
                 if(board[i][j]==1){
-                if(c<2 or c>3 ) v[i][j]=0;
-                else v[i][j]=1;
+                if(c<2 or c>3)
+                    board[i][j]=3;  //newly dead
                 }
-                else
+                else if(board[i][j]==0)
                 {
-                    if(c==3) v[i][j]=1;
+                    if(c==3) board[i][j]=2; //newly alive
                 }
             }
+            
         }
-        board=v;
+        for(int i=0;i<m;i++)
+        { 
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==3) board[i][j]=0;
+                if(board[i][j]==2) board[i][j]=1;
+            }
+        }
     }
 };
