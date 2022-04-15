f197a7480b54e217aef01d9bfbb2081e37eb8937
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-04-15 10:06:57 +0530

    Time: 23 ms (48.84%), Space: 23.9 MB (74.78%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -11,59 +11,14 @@
  */
 class Solution {
 public:
-    queue<TreeNode*>q,temp;
-   bool done;
-    void fill(TreeNode* r, TreeNode* t)
-    {
-        if(done or !r) return;
-        if(!r->left and t->val<r->val) {
-            r->left=t;
-            done=true;
-            return ;
-        }
-        if(!r->right and t->val>r->val)
-        {
-            r->right=t;
-            done=true;
-            return;
-        }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return NULL;
         
-        if(r->val>t->val)
-        fill(r->left,t);
-        else
-        fill(r->right,t);
-    }
-    TreeNode* trimBST(TreeNode* root, int l, int h) {
-        q.push(root);
-        while(!q.empty())
-        {
-            int sz=q.size();
-            while(sz--)
-            {
-                TreeNode* t=q.front();
-                q.pop();
-                
-                if(t->val>=l and t->val<=h) temp.push(t);
-                    
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
-            }
-        }
+        if(root->val<low) return trimBST(root->right,low,high);
+        if(root->val>high) return trimBST(root->left,low,high);
         
-        if(temp.size()==0) return NULL;
-        TreeNode* r=temp.front();
-         temp.pop();
-        r->left=r->right=NULL;
-       while(!temp.empty())
-       {
-           
-           done=false;
-           TreeNode* t=temp.front();
-           temp.pop();
-           
-           t->left=t->right=NULL;
-           fill(r,t);
-       }
-        return r;
+        root->left=trimBST(root->left,low,high);
+        root->right=trimBST(root->right,low,high);
+        return root;
     }
 };
