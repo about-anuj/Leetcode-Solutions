f488b4afe4283d12d414eb61b1a7ef47605d3fd4
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-05-10 22:22:34 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (13.23%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -3,27 +3,27 @@ public:
     vector<vector<int>>ans;
     void calc(int k, int n, int cur,vector<int>&v,int sum)
     {
-        if(sum==n and v.size()==k)
+        if(k==0)
         {
-            if(find(ans.begin(),ans.end(),v)==ans.end())
-            ans.push_back(v);
-            return;
+            if(accumulate(begin(v),end(v),0)==n)
+              ans.push_back(v);
+            else
+                return;
         }
         
-        if(v.size()==k and (sum<n or sum>n)) return;
-        
-        for(int i=cur;i<=9;i++)
-        {
-            v.push_back(cur);
-            calc(k,n,i+1,v,sum+cur); 
-            v.pop_back();
-        }
+        if(cur>9 ) return ;
+        v.push_back(cur); //include cur
+        calc(k-1,n,cur+1,v,sum);
+        v.pop_back();   //not include cur
+        calc(k,n,cur+1,v,sum);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         
         vector<int>v;
-        for(int i=1;i<=9;i++)
-        calc(k,n,i,v,0);
+        //for(int i=1;i<=9;i++)
+        calc(k,n,1,v,0);
+        sort(begin(ans),end(ans));
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
         return ans;
     }
 };
