bea8a8cfcb800d6b6b79adf26fba1189a8a8359a
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-05-16 15:59:28 +0530

    Time: 96 ms (53.87%), Space: 19.2 MB (93.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,35 +1,55 @@
 class Solution {
 public:
+    bool isvalid(vector<vector<int>>&g,int i,int j,int n)
+    {
+        if(i<n and i>=0 and j<n and j>=0 and g[i][j]==0) return 1;
+        return 0;
+    }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size(),ans=0;
+        //vector<vector<int>>temp=grid;
         queue<pair<int,int>>q;
+        if(grid[0][0]==1 or grid[n-1][n-1]==1) return -1;
         
         q.push({0,0});
         
         while(!q.empty())
         {
-            queue<pair<int,int>>q1;
+            int sz=q.size();
             ans++;
-            
-            while(!q.empty())
+            while(sz--)
             {
+                //direction
+              //  {1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}
+                int x[]={1,1,0,0,-1,-1,1,-1};
+                int y[]={1,0,1,-1,0,-1,-1,1};
+                
                 int a=q.front().first;
                 int b=q.front().second;
+                if(a==n-1 and b==n-1) return ans;
                 q.pop();
                 
-                if(a<n and a>=0 and b<n and b>=0 and grid[a][b]==0)
+                for(int j=0;j<8;j++)
                 {
-                    grid[a][b]=1;
-                    if(a==n-1 and b==n-1) return ans;
-                       
-                    for(int i=-1;i<2;i++)
-                       for(int j=-1;j<2;j++)
-                          if(i!=0 or j!=0)
-                            q1.push({a+i,b+j});
-                 }
+                    int nx=a+x[j];
+                    int ny=b+y[j];
+                
+              //      cout<<a<<" "<<b<<endl;
+               // if(a==n-1 and b==n-1) return ans+2;
+                   
+                    if(isvalid(grid,nx,ny,n) and grid[nx][ny]==0)
+                    {
+                    grid[nx][ny]=1;
+                    q.push({nx,ny});
+                    }
+               
                 }
-            swap(q,q1);
+                
+                
+            }
+            
+          //  ans++;
         }
         return -1;
     }
