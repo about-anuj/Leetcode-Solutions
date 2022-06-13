4423b56857c934b2cf0b5d2b7b9d5b20f151df6a
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-06-13 14:29:23 +0530

    Time: 16 ms (11.39%), Space: 9.3 MB (5.41%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int dp[201][201];
-    int calc(vector<vector<int>>&tr, int n,int i,int j)
-    {
-        if(i==n-1){
-            return dp[i][j]=tr[i][j];
-        }
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+//     int calc(vector<vector<int>>&tr, int n,int i,int j)
+//     {
+//         if(i==n-1){
+//             return dp[i][j]=tr[i][j];
+//         }
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
         
-        return dp[i][j]=tr[i][j]+min(calc(tr,n,i+1,j),calc(tr,n,i+1,j+1));
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        memset(dp,-1,sizeof(dp));
-        return calc(triangle,triangle.size(),0,0);
+//         return dp[i][j]=tr[i][j]+min(calc(tr,n,i+1,j),calc(tr,n,i+1,j+1));
+    // }
+    
+   
+    
+    int minimumTotal(vector<vector<int>>& tr) {
+        vector<vector<int>>dp(tr.size(),vector<int>(tr.size(),0));
+        int ans=INT_MAX;
+        dp[0][0]=tr[0][0];
+        for(int i=1;i<tr.size();i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0) dp[i][j]=tr[i][j]+dp[i-1][j];
+                else if(j==i) dp[i][j]=tr[i][j]+dp[i-1][j-1];
+                else dp[i][j]=tr[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
+            }
+        }
+        for(auto i:dp[tr.size()-1])
+            ans=min(i,ans);
+        
+        return ans;
     }
 };
