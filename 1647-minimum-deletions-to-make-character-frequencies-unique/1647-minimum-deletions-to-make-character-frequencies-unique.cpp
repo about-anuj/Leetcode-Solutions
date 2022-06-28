b8fe5d609ec6e6216323981ca529937290e660ab
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-06-28 11:30:21 +0530

    Time: 111 ms (36.90%), Space: 17.4 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<int,int>f1,f2;
+        unordered_map<int,int>f1;
+        priority_queue<int>pq;
         int ans=0;
         for(auto i:s)
             f1[i-'a']++;
         
-        for(auto i:f1){
-                int j=i.second;
-                //iterate till we seen unseen freq
-                while(f2.count(j)){
-                    j--;
-                    ans++;
-                }
-            if(j!=0)
-              f2[j]++;
-            }
+        for(auto i:f1) pq.push(i.second);
         
+        
+        while(pq.size()>1){
+            int t1=pq.top(); pq.pop();
+            
+           // cout<<t1<<" "<<ans<<" "<<pq.size()<<endl;
+            //check that top two elements are equal or not
+            if(t1==pq.top()){
+                ans++;
+                //if updated freq 0 then no need to push
+                if(t1-1>0)
+                pq.push(t1-1);
+            } 
+        }
         return ans;
     }
 };
