9c05429165777ed9db104b5f6f62250ed46db03e
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-06-29 13:04:25 +0530

    Time: 250 ms (31.86%), Space: 12.6 MB (36.75%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -4,22 +4,16 @@ public:
     {
         if(a[0]==b[0])
             return a[1]<b[1];
+        
         return a[0]>b[0] ;
     }
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         vector<vector<int>>ans;
         sort(begin(people),end(people),cmp);
        
-        // for(auto i:people)
-        //     cout<<i[0]<<" "<<i[1]<<endl;
-        
-        for(int i=0;i<people.size();i++)
-        {
-            if(people[i][1]==ans.size())
-                ans.push_back(people[i]);
-            else
-                ans.insert(begin(ans)+people[i][1],people[i]);
-        }
+        for(auto i:people)
+            ans.insert(begin(ans)+i[1],i);
+
         return ans;
     }
 };
