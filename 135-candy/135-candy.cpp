59b0aed439ec0038646ace2a4c4e4f5047b9f527
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-07-04 21:33:49 +0530

    Time: 105 ms (6.80%), Space: 20.3 MB (6.87%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int j=0,ans=0,n=ratings.size();
-       vector<int>l2r(ratings.size(),1);
-       priority_queue<pair<int,int>>pq;
+     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        int ans=0,n=ratings.size();
         
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1])
-                l2r[i]=l2r[i-1]+1;
+        vector<int>candy(n,-1);
+        for(auto i:ratings)
+            pq.push({i,ans++});
+        ans=0;
+        while(!pq.empty()){
+            int ind=pq.top().second;
+            int val=pq.top().first;
+            pq.pop();
+            
+            int mx=1;
+            if(ind>0 and ratings[ind]>ratings[ind-1])
+                mx=max(mx,candy[ind-1]+1);
+            if(ind<n-1 and ratings[ind]>ratings[ind+1])
+                mx=max(mx,candy[ind+1]+1);
+            
+            candy[ind]=mx;
+            ans+=mx;
         }
-        for(int i=n-2;i>=0;i--)
-            if(ratings[i]>ratings[i+1])
-                l2r[i]=max(l2r[i],l2r[i+1]+1);
-        
-        
-        return accumulate(begin(l2r),end(l2r),0);
+        return ans;
     }
 };
