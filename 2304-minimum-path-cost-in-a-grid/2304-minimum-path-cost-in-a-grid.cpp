f17ab7a52b24ab3aaf2daad670e04502a5ce3350
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-07-12 14:13:27 +0530

    Time: 405 ms (83.39%), Space: 80 MB (97.15%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    //int dp[10001][10001];
-    int calc(vector<vector<int>>&gr,vector<vector<int>>&cost,int i, int j, int m, int n,vector<vector<int>>&dp){
+    int dp[101][101];
+    int calc(vector<vector<int>>&gr,vector<vector<int>>&cost,int i, int j, int m, int n){
         if(i==m-1) return gr[i][j];
         
         if(dp[i][j]!=-1) return dp[i][j];
-        
         int ans=INT_MAX;
         for(int k=0;k<n;k++)
-        ans= min(ans,gr[i][j]+cost[gr[i][j]][k]+calc(gr,cost,i+1,k,m,n,dp));
+        ans= min(ans,gr[i][j]+cost[gr[i][j]][k]+calc(gr,cost,i+1,k,m,n));
     
      return dp[i][j]=ans;
     }
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
-         int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-       
+     int m=grid.size(),n=grid[0].size();
+        memset(dp,-1,sizeof(dp));
         int ans=INT_MAX;
         for(int i=0;i<n;i++)
-        ans= min(calc(grid,moveCost,0,i,m,n,dp),ans);
+        ans= min(calc(grid,moveCost,0,i,m,n),ans);
         
         return ans;
     }
