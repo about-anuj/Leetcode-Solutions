1f175e78301387937f78ec9e88d7c29017fec9da
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-07-30 10:53:14 +0530

    Time: 958 ms (13.71%), Space: 161.8 MB (20.23%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string>ans;
+        unordered_map<char,int>freq;
+        int ind=0;
+        
+        for(auto i:words2)
+        {
+            unordered_map<char,int>t;
+            for(auto j:i)
+            {
+                t[j]++;
+                //store max freq and if that char is present in the word1 then it should have greater or                     //equal freq
+                if(freq.count(j))
+                freq[j]=max(freq[j],t[j]);
+                else
+                    freq[j]=t[j];
+            }
+            
+        }
+        
+        for(auto i:words1)
+        {
+            unordered_map<char,int>temp;
+            bool ok=true;
+            ind=0;
+            
+            for(auto j:i)
+                temp[j]++;
+            
+            for(auto j:freq){
+                if(temp[j.first]<j.second){
+                    ok=false;
+                    break;
+                }
+            }
+            if(ok) ans.push_back(i);
+        }
+        return ans;
+    }
+};
