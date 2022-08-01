fa21c832afdb361c4db7e285978dda9f37b342f8
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-08-01 21:20:51 +0530

    Time: 221 ms (100.00%), Space: 104.9 MB (100.00%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    void dfs(int i, int dist, vector<int>& e, vector<int>& memo) {
-    while (i != -1 && memo[i] == -1) {
-        memo[i] = dist++;
-        i = e[i];
-    }
-}
-int closestMeetingNode(vector<int>& e, int node1, int node2) {
-    int res = -1, min_dist = INT_MAX;
-    vector<int> m1(e.size(), -1), m2(e.size(), -1);
-    dfs(node1, 0, e, m1);
-    dfs(node2, 0, e, m2);
-    for (int i = 0; i < e.size(); ++i)
-        if (min(m1[i], m2[i]) >= 0 && max(m1[i], m2[i]) < min_dist) {
-            min_dist = max(m1[i], m2[i]);
-            res = i;
+    void dfs(vector<int>edges,int node,vector<int>& memo){
+        int dist=0;
+        while(node!=-1 and memo[node]==-1)
+        {
+            memo[node]=dist++;
+            node=edges[node];
         }
-    return res;
-}
+    }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+         int n=edges.size(),ans_node=-1,min_dist=INT_MAX;
+         vector<int>m1(n,-1),m2(n,-1);
+         dfs(edges,node1,m1);
+         dfs(edges,node2,m2);
+        
+         for(int i=0;i<edges.size();i++)
+         {
+             //current node should be reachable from both the nodes
+             if(m1[i]>=0 and m2[i]>=0)
+             {
+                 
+                 if(min_dist>max(m1[i],m2[i]))
+                 {
+                     min_dist=max(m1[i],m2[i]);
+                     ans_node=i;
+                 }
+             }
+         }
+        return ans_node;
+    }
 };
