a79e23e3bf97d57bc7ddef4820603cd09ef7e7a2
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-08-04 22:54:07 +0530

    Time: 43 ms (52.36%), Space: 10.1 MB (47.25%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -2,33 +2,24 @@ class Solution {
     int k;
 public:
     vector<vector<int>>ans;
-    
-    void calc(vector<int>&nums,vector<int>&v,int n,unordered_set<int>&st,int j)
+    vector<int>v;
+    void calc(int j,int k,int n)
     {
-        if(v.size()==k)
+        if(k==0)
         {
             ans.push_back(v);
             return;
         }
         
-        for(int i=j;i<nums.size();i++)
+        for(int i=j;i<=n;i++)
         {
-            if(st.find(i)==st.end())
-            {
-                st.insert(i);
-                v.push_back(nums[i]);
-                calc(nums,v,n,st,i+1);
-                st.erase(i);
+                v.push_back(i);
+                calc(i+1,k-1,n);
                 v.pop_back();
-            }
         }
     }
     vector<vector<int>> combine(int n, int k) {
-       vector<int>nums,v;
-        unordered_set<int>st;
-        this->k=k;
-        for(int i=1;i<=n;i++) nums.push_back(i);
-        calc(nums,v,n,st,0);
+        calc(1,k,n);
         return ans;
         
     }
