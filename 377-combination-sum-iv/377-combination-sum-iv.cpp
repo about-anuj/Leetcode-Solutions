64483fd31757fd5a43380bd6c6cc626cedd17f62
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-08-05 19:09:16 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int dp[1001];
-    int calc(vector<int>&nums,int tgt)
+    int help(vector<int>&nums,int tgt)
     {
         if(tgt==0) return 1;
         if(dp[tgt]!=-1) return dp[tgt];
         
-        dp[tgt]=0;
-        for(auto i:nums)
-            if(tgt-i>=0)
-                dp[tgt]+=calc(nums,tgt-i);
+        int way=0;
+        for(auto &n:nums)
+            if(tgt-n>=0)
+                way+=help(nums,tgt-n);
         
-        return dp[tgt];
+        return dp[tgt]=way;
     }
     int combinationSum4(vector<int>& nums, int target) {
-       memset(dp,-1,sizeof(dp));
-            return calc(nums,target);
+        memset(dp,-1,sizeof(dp));
+        return help(nums,target);
     }
 };
