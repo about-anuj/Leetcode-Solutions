52dbef961c9f936f734c83f50417e49b2b40d91d
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-08-08 22:43:46 +0530

    Time: 2479 ms (5.00%), Space: 293 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp(nums.size(),INT_MAX);
-        for(auto i:nums){
-            auto it=lower_bound(begin(dp),end(dp),i)-begin(dp);
-            if(dp[it]>=i)
-                dp[it]=i;
+        int k=nums.size();
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+	    for(int i = 0; i <= k; i++) {
+		    //ans[i] = new int[k+1];
+		    for(int j = 0; j <= k; j++) {
+		    	dp[i][j] = -1;
+		    }
+	    }
+        return lis(-1,0,nums,dp);
+    }
+    int lis(int prev,int curr,vector<int>& arr,vector<vector<int>>&dp){
+        if(curr==arr.size()){
+            return 0;
+        } 
+        if(prev!=-1 && dp[prev][curr]!=-1){
+            return dp[prev][curr];
         }
-        for(int i=0;i<nums.size();i++)
-            if(dp[i]==INT_MAX)
-                return i;
-        return (int)nums.size();
+        int first=0;
+        if(prev==-1 || arr[prev]<arr[curr]){
+            first=1+lis(curr,curr+1,arr,dp);
+        }
+        int sec=lis(prev,curr+1,arr,dp);
+        if(prev!=-1){
+            return dp[prev][curr]=max(first,sec);
+        }else
+            return max(first,sec);
     }
 };
