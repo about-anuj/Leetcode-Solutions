1bd59af686abf9d8d38b57fb79799cc7a75382b3
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-08-19 11:57:44 +0530

    Time: 194 ms (35.54%), Space: 58.3 MB (58.44%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-   
     bool isPossible(vector<int>& nums) {
-        vector<vector<int>>v;
-       // each integer is exactly one more than the previous integer
-        for(int i=0;i<nums.size();i++)
+        
+        
+        //we have to check that before starting new subsequence can current element join a 
+        //already formed subsequence or it need to create new
+        unordered_map<int,int>left,end;
+        for(auto i:nums) left[i]++;
+        for(auto i:nums)
         {
-         //   sort(begin(v),end(v),cmp);
-            int k;
-            for(k=v.size()-1;k>=0;k--)
+            if(!left[i]) continue;
+            
+            left[i]--;
+            //can it join existed subsequence
+            if(end[i-1]>0){
+                end[i-1]--;
+                end[i]++;
+            }//if not than can it create new subsequence of atleast three element including itself
+            else if(left[i+1] and left[i+2])
             {
-                if(v[k].back()+1==nums[i])
-                {
-                    v[k].push_back(nums[i]);
-                    break;
-                }   
+                //we have added next two element so exclude them from left 
+                left[i+1]--;
+                left[i+2]--;
+                end[i+2]++; //ending of list is at i+2 
             }
-            if(k==-1)
-              v.push_back({nums[i]});
+            else //if cant join or cant make then it is not possible to add that element to any list
+                return false;
+            
         }
-        
-        for(auto i:v)
-           if(i.size()<3) return false;
-        
         return true;
     }
 };
