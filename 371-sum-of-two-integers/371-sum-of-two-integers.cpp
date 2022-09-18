90e004cd161a7b762ae1bba93f8a91ee1b9f4a70
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-09-18 22:03:05 +0530

    Time: 5 ms (11.54%), Space: 5.8 MB (74.20%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+//         int x=0,carry=0;
+//         for(int i=0;i<14;i++)
+//         {
+//             if((a<<i) & (b<<i))
+//                 carry++;
+//             else
+//             {
+                 
+//             }
+//         }
+        return a+b;
+    }
+};
