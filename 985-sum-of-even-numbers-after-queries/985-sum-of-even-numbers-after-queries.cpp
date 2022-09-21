c879185ddb9e329ed3a22a887975e4cd351afbdc
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-09-21 09:38:02 +0530

    Time: 314 ms (5.16%), Space: 49.1 MB (24.21%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int even=0,odd=0,j=0;
+        int even=0,j=0;
         vector<int>res(nums.size(),0);
         for(auto i:nums)
             if(i%2==0) even+=i;
-            else odd+=i;
         
         for(auto i:queries)
         {
-          if((nums[i[1]]+i[0])%2==0)
-          {
-              if(nums[i[1]]%2==0)
-                  even+=i[0];
-              else
-              {
-                  odd-=nums[i[1]];
-                  even+=(nums[i[1]]+i[0]);
-              }
-          }
-          else
-          {
-              if((nums[i[1]])%2==0)
-              {
-                  even-=nums[i[1]];
-                  odd+=nums[i[1]]+i[0];
-              }
-          }
+           //we don't care what it was just remove from even if it was even
+            if(nums[i[1]]%2==0) even-=nums[i[1]];
+            //now add the number
             nums[i[1]]+=i[0];
+            
+            //if it is now even then include it
+            if(nums[i[1]]%2==0)
+                even+=nums[i[1]];
+            
             res[j++]=even;
         }
         return res;
