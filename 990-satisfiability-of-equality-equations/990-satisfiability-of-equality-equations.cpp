1ecac4690dc851fff4aa57ec96608003d43e3031
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-09-26 22:49:59 +0530

    Time: 11 ms (53.23%), Space: 11 MB (99.93%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,50 +1,40 @@
-static bool found=false;
 class Solution {
 public:
-    unordered_map<char,vector<char>>g;
-    
-    void dfs(char st,vector<int>&vis,char tgt)
+    int dsu[26];
+    int find_parent(int c)
     {
-        if(vis[st-'a'] or found) return;
-        vis[st-'a']=1;
-        if(st==tgt)
-        {
-            found=true;
-            return;
-        }
-        for(auto i:g[st])
-        {
-            if(!vis[i-'a'])
-                dfs(i,vis,tgt);
-        }
+        // char org=c;
+        while(dsu[c]!=c)
+           c=dsu[c];
+        
+        // cout<<c<<" ";
+        return c;
     }
     bool equationsPossible(vector<string>& eq) {
-        int vis[26]={0};
+        // dsu.resize(26,0);
+        //set parent as same
+        for(auto i='a';i<='z';i++) dsu[i-'a']=i-'a';
         
         for(auto i:eq)
         {
-            //if sign is == then we have to add both character into same graph
-            //else they will not be part of each other
-                if(i.substr(1,2)=="==")
-                {
-                   g[i[0]].push_back(i[3]);
-                   g[i[3]].push_back(i[0]);
-                     vis[i[0]-'a']=1;
-                     vis[i[3]-'a']=1;
-                }
-               
+            if(i[1]=='=')
+            {
+                int x=find_parent(i[0]-'a');
+                int y=find_parent(i[3]-'a');
+                //set parent
+                dsu[x]=y;
+            }
         }
-      
+        // for(auto i:dsu)cout<<i<<" ";
         for(auto i:eq)
         {
-            vector<int>v(26,0);
-                found=false;
-                dfs(i[0],v,i[3]);
-            // if sign is == then found should be true because both are supposed to part of same graph
-                if(i.substr(1,2)=="==" and !found)
-                    return 0;
-                else if(i.substr(1,2)=="!=" and found) 
-                    return 0;
+            if(i[1]=='!')
+            {
+                int x=find_parent(i[0]-'a');
+                int y=find_parent(i[3]-'a'); 
+                if(x==y) return 0;
+            }
+                
         }
         return 1;
     }
