3bf038b6962dc3ae1cd6b3e0d454824af7ca5d2a
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-10-01 15:53:24 +0530

    Time: 6 ms (29.19%), Space: 6.1 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -3,22 +3,22 @@ public:
     int dp[105];
     int calc(string &s,int i,int n)
     {
+        //completed whole string
         if(i>=n) return dp[i]=1;
+        //leading zeroes are not allowed
         if(s[i]=='0') return dp[i]=0;
         if(dp[i]!=-1) return dp[i];
-        //if i<n-1 then we have two options we can include numbers from 11 to 26
+        
         int x=0,y=0;
         if(i<n-1)
         {
           int num=stoi(s.substr(i,2));
+          //if i<n-1 then we have two options we can include numbers from 11 to 26
           if(num>0 and num<=26)
               x+=calc(s,i+2,n);
-            
-          int n2=s[i]-'0';
-            x+=calc(s,i+1,n);
         }
-        else 
-            y+=calc(s,i+1,n);
+        //we always have one option to include single element
+         y+=calc(s,i+1,n);
         return dp[i]=x+y;
     }
     int numDecodings(string s) {
