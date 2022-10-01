a47ee27bdea844c0638bf52dcddf6444b833f6e5
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-10-01 15:49:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (93.04%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[105];
+    int calc(string &s,int i,int n)
+    {
+        if(i>=n) return dp[i]=1;
+        if(s[i]=='0') return dp[i]=0;
+        if(dp[i]!=-1) return dp[i];
+        //if i<n-1 then we have two options we can include numbers from 11 to 26
+        int x=0,y=0;
+        if(i<n-1)
+        {
+          int num=stoi(s.substr(i,2));
+          if(num>0 and num<=26)
+              x+=calc(s,i+2,n);
+            
+          int n2=s[i]-'0';
+            x+=calc(s,i+1,n);
+        }
+        else 
+            y+=calc(s,i+1,n);
+        return dp[i]=x+y;
+    }
+    int numDecodings(string s) {
+        memset(dp,-1,sizeof(dp));
+        return calc(s,0,s.size());
+    }
+};
