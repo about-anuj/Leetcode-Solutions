5b719ed39f5ea26a310b54348578abb5a66b4e57
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-10-02 11:49:01 +0530

    Time: 30 ms (75.89%), Space: 6 MB (87.41%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int dp[31][31][1001];
+    int dp[31][1001];
     const int mod=1e9+7;
     int calc(int n,int k,int tgt)
     {
         if(tgt<0) return 0;
-        if(n==0 or tgt==0) return dp[n][k][tgt]=(n==tgt);
+        if(n==0 or tgt==0) return dp[n][tgt]=(n==tgt);
         
-        if(dp[n][k][tgt]!=-1) return dp[n][k][tgt];
+        if(dp[n][tgt]!=-1) return dp[n][tgt];
      
         long long ways=0;
         for(int i=1;i<=k;i++)
         {
             ways=(ways+calc(n-1,k,tgt-i))%mod;
         }
-        return dp[n][k][tgt]=ways;
+        return dp[n][tgt]=ways;
     }
     int numRollsToTarget(int n, int k, int target) {
         memset(dp,-1,sizeof(dp));
