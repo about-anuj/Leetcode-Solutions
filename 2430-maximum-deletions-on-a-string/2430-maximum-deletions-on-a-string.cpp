5c6626083ae751fe4091ef592c5ed32998496f41
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-10-02 11:26:44 +0530

    Time: 1604 ms (12.50%), Space: 6.7 MB (87.50%) - LeetHub

--- 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    
-   int dp[40000] = {};
-int dfs(int i, string &s) {
-    if (dp[i] == 0) {
-        dp[i] = 1;
-        for (int len = 1; i + 2 * len <= s.size(); ++len)
-            if (s.compare(i, len, s, i + len, len) == 0)
-                dp[i] = max(dp[i], 1 + dfs(i + len, s));
+    int dp[10005];
+    int calc(int i,string &s)
+    {
+        if(dp[i]!=-1) return dp[i];   
+        int x=1;
+        for(int len=1;i+2*len<=s.size();len++)
+        {
+           if (s.compare(i, len, s, i + len, len) == 0)
+                x = max(x, 1 + calc(i + len, s)); 
+        }
+        return dp[i]=x;
     }
-    return dp[i];
-}
-int deleteString(string s) {
-    return dfs(0, s);
-}
+    int deleteString(string s) {
+        memset(dp,-1,sizeof(dp));
+        return calc(0,s);
+    }
 };
+// int dp[40000] = {};
+// int dfs(int i, string &s) {
+//     if (dp[i] == 0) {
+//         dp[i] = 1;
+//         for (int len = 1; i + 2 * len <= s.size(); ++len)
+//             if (s.compare(i, len, s, i + len, len) == 0)
+//                 dp[i] = max(dp[i], 1 + dfs(i + len, s));
+//     }
+//     return dp[i];
+// }
+// int deleteString(string s) {
+//     return dfs(0, s);
+// }
