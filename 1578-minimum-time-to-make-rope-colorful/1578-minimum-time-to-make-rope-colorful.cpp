8e70f352136916d51258eccf324c6e7f5f1d1f99
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-10-03 09:18:23 +0530

    Time: 162 ms (94.21%), Space: 95.5 MB (19.54%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -6,17 +6,18 @@ public:
         for(int i=0;i<colors.size();)
         {
             char c=colors[i];
-            int mn=0,j=i,sm=0;
+            int mx=0,j=i,sm=0;
+            //find out total sum and release all element except max element
             while(i<colors.size() and colors[i]==c)
             {
                 sm+=needTime[i];
-                mn=max(mn,needTime[i]);
+                mx=max(mx,needTime[i]);
                 i++;
             }
             //it means there are more duplicates
             if(i-j>1)
             {
-                res+=(sm-mn);  //only take minimum number from them
+                res+=(sm-mx);  //only take maximum number from them it will help to decrease overall cost
             }
         }
         return res;
