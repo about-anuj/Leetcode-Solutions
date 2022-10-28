093895e7b299c2c0fd17f8c6547a970cab15cc7c
anuj <63638503+anujsh2002@users.noreply.github.com>
2022-10-28 14:04:13 +0530

    Time: 603 ms (19.23%), Space: 113.9 MB (23.76%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -8,9 +8,6 @@ public:
         for(auto i=0;i<nums.size();i++)
         {
             sum=(sum+nums[i])%k;
-            // if(sum==0) return true;
-            // (sum+x)%k==0
-            
             if(mp.count(sum) and i-mp[sum]>=2) return true;
             else if(mp.count(sum)==0)  mp[sum]=i;
             
@@ -18,22 +15,3 @@ public:
         return 0;
     }
 };
-// runningSum+=nums[i];
-            
-//             if(k!=0) 
-//                 runningSum = runningSum%k;
-            
-//             //check if the runningsum already exists in the hashmap
-//             if(mp.find(runningSum)!=mp.end())
-//             {
-//                 //if it exists, then the current location minus the previous location must be greater than1
-                
-//                 if(i-mp[runningSum]>1)
-//                     return true;
-//             }
-//             else
-//             {
-//                 //otherwise if the current runningSum doesn't exist in the hashmap, then store it as it maybe used later on
-                
-//                 mp[runningSum]=i;
-//             }
