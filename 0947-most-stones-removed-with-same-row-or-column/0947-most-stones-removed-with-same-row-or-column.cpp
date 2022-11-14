28fdb70ce0cc0871dbb643a39d11a286080dee22
anuj <63638503+about-anuj@users.noreply.github.com>
2022-11-14 14:54:15 +0530

    Time: 1075 ms (5.01%), Space: 18.7 MB (41.68%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     unordered_map<int,vector<int>>mp;
-    int dfs(int src,vector<int>&vis)
+    void dfs(int src,vector<int>&vis)
     {
-        if(vis[src]==1) return 0;
+        if(vis[src]==1) return ;
         vis[src]=1;
         
-        int depth=1;
+        // int depth=1;
         for(auto i:mp[src])
         {
             if(vis[i]==0)
-                depth+=dfs(i,vis);
+                dfs(i,vis);
         }
-        return depth;
         
     }
     int removeStones(vector<vector<int>>& stones) {
@@ -31,9 +30,12 @@ public:
         }
         for(int i=0;i<stones.size();i++)
         {
-            if(vis[i]==0)
-            ans+=dfs(i,vis)-1;
+           if(vis[i]==0)
+           {
+               n--;
+               dfs(i,vis);
+           }
         }
-        return ans;
+        return n;
     }
 };
